5f498cf16c2678f11e6ff608e20fd75821fbb413
Rajib Chowdhury <rajib.a.chowdhury@accenture.com>
2019-01-23 12:43:14 +1100

    list instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,19 +4,30 @@ import click
 session = boto3.Session(profile_name='acg-python')
 ec2 = session.resource('ec2')
 
-@click.command()
 
-def list_instances():
+@click.command()
+@click.option('--project', default=None,
+              help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
     "List ec2 instances"
-    for i in ec2.instances.all():
-        print (', '.join((
+    instances = []
+
+    if project:
+        filters = [{'Name': 'tag:Project', 'Values': [project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        print(', '.join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
             i.private_dns_name
-            ))  )
+        )))
     return
+
 
 if __name__ == '__main__':
     list_instances()
